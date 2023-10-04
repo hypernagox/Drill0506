dce8f70f19f38e44dde433668c2608ffb02d27ae
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-10-04 16:25:26 +0900

    Reformat

--- drill05_solution.py
+++ drill05_solution.py
@@ -3,6 +3,7 @@ from pico2d import *
 from pico2d import *
 import random
 
+
 def load_resources():
     global TUK_ground, character
     global arrow
@@ -26,7 +27,7 @@ def handle_events():
 def reset_world():
     global running, cx, cy, frame
     global hx, hy
-    global sx,sy
+    global sx, sy
     global t
     global action
 
@@ -35,15 +36,16 @@ def reset_world():
     frame = 0
     action = 3
 
-    sx, sy = cx, cy # p1: 시작점
-    #hx, hy = TUK_WIDTH - 50 , TUK_HEIGHT - 50
-    hx, hy = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT) # p2: 끝점
+    sx, sy = cx, cy  # p1: 시작점
+    # hx, hy = TUK_WIDTH - 50 , TUK_HEIGHT - 50
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2: 끝점
     t = 0.0
+
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx,hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
@@ -57,11 +59,11 @@ def update_world():
     frame = (frame + 1) % 8
     action = 1 if cx < hx else 0
 
-
     if t <= 1.0:
-        cx = (1-t)*sx + t*hx # cx는 시작 x 와 끝 x 를 1-t:t의 비율로 섞은 위치
-        cy = (1-t)*sy + t*hy
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x 를 1-t:t의 비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
         t += 0.001
+
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
